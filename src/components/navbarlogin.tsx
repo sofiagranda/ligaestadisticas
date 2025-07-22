123cae1e9aeaba698069a206e17bea4046174519
copa-los-de-solanda <cfm.10c.nicolasparedes@gmail.com>
2025-07-22 13:34:27 -0500

    actua

--- src/components/navbarlogin.tsx
+++ src/components/navbarlogin.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import './navbarlogin.css'
+import './navbarlogin.css';
 
 const NavbarLogin: React.FC = () => {
   const { username } = useAuth();
+  const [menuOpen, setMenuOpen] = useState(false); // estado para toggle
 
   if (!username) return null; // No mostrar si no está logueado
 
   return (
     <nav className="navbar">
-      <ul className="navbar__list">
+      <button className="navbar__toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+      <ul className={`navbar__list ${menuOpen ? 'show' : ''}`}>
         <li><Link to="/jugadores">Jugadores</Link></li>
         <li><Link to="/equipos">Equipos</Link></li>
         <li><Link to="/partidos">Partidos</Link></li>
